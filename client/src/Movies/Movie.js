cbd0b44f44f61fdc73c8ef92df0ff1e5e0048f01
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-07-22 23:19:07 +0100

    WIP: completed stretch task: to refactor so that our code is DRY

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import MovieCard from './MovieCard'
 
 const Movie = (props) => {
   const [movie, setMovie] = useState({});
   // const id = props.match.params.id
-  // const 
  
   useEffect(() => {
     const id = props.match.params.id;
@@ -15,6 +15,7 @@ const Movie = (props) => {
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
           setMovie(response.data);
+          console.log(response.data)
         })
         .catch(error => {
           console.error(error);
@@ -32,27 +33,28 @@ const Movie = (props) => {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
+  // const { title, director, metascore, stars } = movie;
   return (
-    <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {/* {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))} */}
-      </div>
-      <div className="save-button">Save</div>
-    </div>
+    // <div className="save-wrapper">
+    //   <div className="movie-card">
+    //     <h2>{title}</h2>
+    //     <div className="movie-director">
+    //       Director: <em>{director}</em>
+    //     </div>
+    //     <div className="movie-metascore">
+    //       Metascore: <strong>{metascore}</strong>
+    //     </div>
+    //     <h3>Actors</h3>
+    //     <p>{stars}</p>
+    //     {/* {stars.map(star => (
+    //       <div key={star} className="movie-star">
+    //         {star}
+    //       </div>
+    //     ))} */}
+    //   </div>
+    //   <div className="save-button">Save</div>
+    // </div>
+    <MovieCard key={movie.id} title={movie.title} director={movie.director} metascore={movie.metascore} stars={movie.stars}></MovieCard>
   );
 }
 
