3ed987b49875a3b6cd1e0ffe5a2102ed45243960
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-07-22 23:22:42 +0100

    WIP: trying to figure out 'stars.map' bug in MVP, otherwise complete

--- client/src/App.js
+++ client/src/App.js
@@ -15,11 +15,8 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      {/* <div>Replace this Div with your Routes</div> */}
       <Route path="/" exact component={MovieList} />
       <Route path="/movies/:id" component={Movie} />
-      {/* <Route path={`/Movies/1`} component={Movie} /> */}
-
     </div>
   );
 };
--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,26 +1,26 @@
-import React from 'react';
+// import React from 'react';
 
-const MovieCard = props => {
-  return (
-  <div className="save-wrapper">
-  <div className="movie-card">
-    <h2>{props.title}</h2>
-    <div className="movie-director">
-      Director: <em>{props.director}</em>
-    </div>
-    <div className="movie-metascore">
-      Metascore: <strong>{props.metascore}</strong>
-    </div>
-    <h3>Actors</h3>
-    <p>{props.stars}</p>
-    {/* {stars.map(star => (
-      <div key={star} className="movie-star">
-        {star}
-      </div>
-    ))} */}
-  </div>
-  <div className="save-button">Save</div>
-</div>
-)};
+// const MovieCard = props => {
+//   return (
+//   <div className="save-wrapper">
+//   <div className="movie-card">
+//     <h2>{props.title}</h2>
+//     <div className="movie-director">
+//       Director: <em>{props.director}</em>
+//     </div>
+//     <div className="movie-metascore">
+//       Metascore: <strong>{props.metascore}</strong>
+//     </div>
+//     <h3>Actors</h3>
+//     <p>{props.stars}</p>
+//     {/* {stars.map(star => (
+//       <div key={star} className="movie-star">
+//         {star}
+//       </div>
+//     ))} */}
+//   </div>
+//   <div className="save-button">Save</div>
+// </div>
+// )};
 
-export default MovieCard;
+// export default MovieCard;
